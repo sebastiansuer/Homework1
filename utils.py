9e3ac1df30e48780a0486ead74dc92976f88b93b
Sebastian Suer <sebastian@suer.at>
2024-04-07 23:11:44 +0200

    Write calculator function

---
+++ utils.py
@@ -0,0 +1,19 @@
+# This function adds two numbers
+def add(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
+# This function calculates x to the power of y
+def pow(x, y):
+    return x**y
